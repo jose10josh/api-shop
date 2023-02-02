bf1321001663621076d3dc26fe79c2752d024dbd
José Arévalo <joseaf1002@gmail.com>
2023-02-01 21:34:46 -0600

    Fix migrations order

--- db/migrations/20230201223107-add-orders-table.js
+++ db/migrations/20230201223107-add-orders-table.js
@@ -1,13 +1,41 @@
 'use strict';
 
-const {OrderSchema, ORDER_TABLE} = require('../models/order.model');
+const {DataTypes, Sequelize} = require('sequelize');
+
+const {CUSTOMER_TABLE} = require('../models/customer.model.js');
+const { ORDER_TABLE} = require('../models/order.model');
 const {OrdersProducsSchema, ORDER_PRODUCT_TABLE} = require('../models/order-products.model');
 
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        primaryKey:true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true
+      },
+      customerId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        filed: 'customer_id',
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'userId'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW
+      }
+    });
+
     await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrdersProducsSchema);
   },
 
